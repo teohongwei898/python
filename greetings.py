4a02e34c9b5c0f62ee4dbe562f331356bb4e907c
Hong Wei <teohongwei898@gmail.com>
2018-11-12 14:16:07 +0800

    Create greetings.py

---
+++ greetings.py
@@ -0,0 +1,10 @@
+#Write your function here
+def add_greetings(names):
+  for x in names:
+    y= 0
+    print ("Hello, " + names[y])
+    y +=1
+    continue
+
+#Uncomment the line below when your function is done
+print(add_greetings(["Owen", "Max", "Sophie"]))
