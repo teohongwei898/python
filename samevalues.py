1c15d1ab97403b702ec12cbb59c3dd43e8cc90a6
Hong Wei <teohongwei898@gmail.com>
2018-11-12 14:45:04 +0800

    Create samevalues.py

---
+++ samevalues.py
@@ -0,0 +1,7 @@
+#Write your function here
+def same_values(lst1, lst2):
+  new_lst = []
+  for index in range(len(lst1)):
+    if lst1[index] == lst2[index]:
+      new_lst.append(index)
+  return new_lst
